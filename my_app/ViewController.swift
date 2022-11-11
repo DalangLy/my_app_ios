6c01ad57dbb89e92ccc4808aee91bd94ba4125cc
dalangly <dalangly@gmail.com>
2022-11-11 15:14:50 +0700

    update ui

--- my_app/ViewController.swift
+++ my_app/ViewController.swift
@@ -2,49 +2,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let loginButton:UIButton = UIButton();
-
+    let loginButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    let loginHeader: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "Login"
+        label.font = UIFont(name: "Times New Roman", size: 45)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Login"
         self.view.backgroundColor = .systemBackground
         
-        self.setupLogin()
-        self.setupLoginLabel()
-    }
-    
-    func setupLoginLabel(){
-        let loginLabel:UILabel = UILabel()
-        loginLabel.text = "Login"
-        loginLabel.font = UIFont(name: "Times New Roman", size: 45)
-        loginLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(loginLabel)
-        
-        NSLayoutConstraint.activate([
-            loginLabel.topAnchor.constraint(equalTo: view.topAnchor),
-            loginLabel.bottomAnchor.constraint(equalTo: loginButton.topAnchor),
-            loginLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            loginLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
-    }
-    
-    func setupEmailInput(){
-        
+        self.setupUI()
     }
 
-    func setupLogin(){
-        loginButton.setTitle("Login", for: .normal)
-        loginButton.setTitleColor(.blue, for: .normal)
-        loginButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(loginButton)
+    func setupUI(){
+        view.addSubview(loginHeader)
         
         NSLayoutConstraint.activate([
-            loginButton.topAnchor.constraint(equalTo: view.topAnchor),
-            loginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            loginHeader.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            loginHeader.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        self.view.addSubview(loginButton)
+        
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalTo: loginHeader.bottomAnchor, constant: 20),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
 }
-
