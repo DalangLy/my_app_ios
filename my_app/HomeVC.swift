9868a4c795e4e48045a9a7690f683ce52e58ecfb
dalangly <dalangly@gmail.com>
2022-11-14 15:42:28 +0700

    add tabbar

--- my_app/HomeVC.swift
+++ my_app/HomeVC.swift
@@ -1,19 +1,88 @@
 import UIKit
 
-class HomeVC: UIViewController, UICollectionViewDelegate{
+
+class TabOneViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBackground
+        self.title = "Tab 1"
+        
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 50))
+        label.text = "Tab 1"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
+}
+
+class TabTwoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBackground
+        self.title = "Tab 2"
+        
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 50))
+        label.text = "Tab 2"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+class HomeVC: UITabBarController, UICollectionViewDelegate, UITabBarControllerDelegate{
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let cvFlowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout();
-        cvFlowLayout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-        cvFlowLayout.itemSize = CGSize(width: 60, height: 60)
-        let cv: UICollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: cvFlowLayout);
-        cv.register(MyCustomCell.self, forCellWithReuseIdentifier: "myCell")
-        cv.dataSource = self
-        cv.delegate = self
-        cv.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(cv)
+//        let cvFlowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout();
+//        cvFlowLayout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+//        cvFlowLayout.itemSize = CGSize(width: 60, height: 60)
+//        let cv: UICollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: cvFlowLayout);
+//        cv.register(MyCustomCell.self, forCellWithReuseIdentifier: "myCell")
+//        cv.dataSource = self
+//        cv.delegate = self
+//        cv.translatesAutoresizingMaskIntoConstraints = false
+//        self.view.addSubview(cv)
+        
+        
+        
+        
+        //create tab bar
+        let tabOne = TabOneViewController()
+        let tabOneBarItem = UITabBarItem(title: "Tab 1", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house"))
+        
+        tabOne.tabBarItem = tabOneBarItem
+        
+        
+        // Create Tab two
+        let tabTwo = TabTwoViewController()
+        let tabTwoBarItem2 = UITabBarItem(title: "Tab 2", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house"))
+        
+        tabTwo.tabBarItem = tabTwoBarItem2
+        
+        
+        self.viewControllers = [tabOne, tabTwo]
     }
     
     override func viewDidLoad() {
@@ -22,6 +91,10 @@ class HomeVC: UIViewController, UICollectionViewDelegate{
         // Do any additional setup after loading the view.
         self.title = "Home"
         self.view.backgroundColor = .systemBackground
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        print("select");
     }
 }
 
